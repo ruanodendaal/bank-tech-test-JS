775e7ab628963b5af30648b77a04e3bae5add69f
Ruan Odendaal <ruanodendaal@gmail.com>
2017-06-20 18:02:06 +0100

    failing test for making deposits

--- test/AccountSpec.js
+++ test/AccountSpec.js
@@ -4,16 +4,29 @@ const expect = require("chai").expect;
 const Account = require('../app/Account');
 
 describe('Account', function() {
-  describe('Account opening', function() {
 
+  describe('Account opening', function() {
     it('open a new account with zero balance', function() {
       var acc = new Account.Account();
       expect(acc.getBalance()).to.equal(0);
     });
 
-    it('opens a new account with 500 balance', function() {
+    it('open a new account with £500 balance', function() {
       var acc = new Account.Account(500);
       expect(acc.getBalance()).to.equal(500);
+    });
+  });
+
+  describe('Making deposits', function() {
+    var acc;
+
+    beforeEach(function() {
+      acc = new Account.Account();
+    });
+
+    it('make a £100 deposit', function() {
+      acc.deposit(100);
+      expect(acc.balance).to.equal(100);
     });
   });
 });
