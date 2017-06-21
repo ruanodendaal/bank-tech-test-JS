48d4414e796210785f7ef84d6cb15ad23fd2cf82
Ruan Odendaal <ruanodendaal@gmail.com>
2017-06-21 12:37:11 +0100

    failing test for statement print

--- test/StatementSpec.js
+++ test/StatementSpec.js
@@ -4,8 +4,19 @@ const expect = require("chai").expect;
 const Statement = require('../app/Statement');
 
 describe('Statement', function() {
+  var statement;
+
+  beforeEach(function() {
+    statement = new Statement.Statement();
+  });
+
   it('is instantiated with an empty transactions list', function() {
-    var statement = new Statement.Statement();
     expect(statement.transactions).to.be.empty;
+  });
+
+  it('prints a statement', function() {
+    statement.transactions.push({ date: `today.DateToday()`, type: 'deposit', amount: 100 });
+    expect(statement.print()).to.equal("date || credit || debit || balance\n" +
+                                      `${today.DateToday()} || 100.00 || || 3000.00\n`);
   });
 });
