c9ec85a9e21d1139091e3853af16f729a297e0a1
Ruan Odendaal <ruanodendaal@gmail.com>
2017-06-22 15:31:18 +0100

    test deposit and withdrawl independantly

--- test/StatementSpec.js
+++ test/StatementSpec.js
@@ -15,12 +15,17 @@ describe('Statement', function() {
     expect(statement.transactions).to.be.empty;
   });
 
-  it('prints a statement', function() {
-    let trans1 = { date: `${today.DateToday()}`, type: 'deposit', amount: 100, currentBalance: 500 }
-    let trans2 = { date: `${today.DateToday()}`, type: 'withdraw', amount: 10, currentBalance: 490 }
-    statement.transactions.push(trans1, trans2);
+  it('prints deposit on statement', function() {
+    let trans = { date: `${today.DateToday()}`, type: 'deposit', amount: 100, currentBalance: 500 }
+    statement.transactions.push(trans);
     expect(statement.print()).to.equal("date || credit || debit || balance\n" +
-                                      `${today.DateToday()} || || 10 || 490\n` +
                                       `${today.DateToday()} || 100 || || 500\n`);
+  });
+
+  it('prints withdrawl on statement', function() {
+    let trans = { date: `${today.DateToday()}`, type: 'withdraw', amount: 10, currentBalance: 490 }
+    statement.transactions.push(trans);
+    expect(statement.print()).to.equal("date || credit || debit || balance\n" +
+                                      `${today.DateToday()} || || 10 || 490\n`);
   });
 });
